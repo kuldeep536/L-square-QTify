41dff5a82634ea8bfaac004d1a595f8fb15a82bc
Kuldeep Malviya <129328118+kuldeep536@users.noreply.github.com>
2024-07-25 19:46:25 +0530

    commit 9

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-// import Style from "./logo.module.css"
+ import Style from "./logo.module.css"
 //import { styled } from "@mui/material";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo"/>;
+  return <img src={LogoImage} alt="logo" className={Style.CustomLogo}/>;
 }
